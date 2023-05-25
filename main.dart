78c0392336b3dff56cf1e0eb885f7d60a23654d0
ChoJinmok <jjmok1127@gmail.com>
2023-05-25 21:56:34 +0900

    1.3 Nullable Variables

--- main.dart
+++ main.dart
@@ -1,29 +1,35 @@
-// dart는 개발자 친화적인 언어이다.
-// -> 때때로 규칙을 조금 회피할 수 있게 해준다.
+// null safety
+// 개발자가 null값을 참조할 수 없도록 막아주는 기능
+// 만약 null값을 참조하면 런타임(사용자가 프로그램을 사용하는 동안) 에러가 발생한다.
+// -> 컴파일 시점에 에러를 잡아줄 수 있어야 한다: null safety의 목적
 
-// dynamic 키워드: 여러가지 타입을 가질 수 있는 변수를 선언할 때 사용한다.
-// (사용하는게 추천되지는 않지만, 때때로 유용할 때가 있거나 방법이 없는 경우가 있을 때 사용한다.)
+// without null safety
+// bool isEmpty(String string) => string.length == 0;
 
+// main() {
+//   isEmpty(null);
+// }
+
+// => NoSuchMethodError, String을 보내야 하는 곳에 null을 보내면 에러가 발생한다.
+// null에서 length를 호출하려고 하기 때문에 에러가 발생한다.
+// 언어 자체에 null은 필요하다. -> null 자체가 유해한 것이 아니라 null을 참조하려고 할 때 문제가 발생한다.
+
+// dart에서는 변수가 null이 될 수도 있다는 것을 정확히 명시해줘야 한다.
 void main() {
-  // 타입을 명시하지 않고 아무것도 지정하지 않으면 dynamic 타입이 된다.
-  // -> 모든 타입을 가질 수 있는 변수가 된다.
-  var name;
-  name = 'nico';
-  name = 12;
-  name = true;
+  // nico는 String 타입이기 때문에 null을 할당할 수 없다.
+  // String nico = 'nico';
+  // nico = null;
 
-  // dynamic 키워드를 사용해도 된다.
-  dynamic name2;
-
-  // dart에서 dynamic 변수를 보호해주는 방법 제공
-  if (name2 is String) {
-    // name2는 String 타입이다.
+  // 변수가 null이 될 수도 있다는 것을 명시 -> '?' 사용
+  String? nico = 'nico';
+  nico = null;
+  // nico.length; -> null safety(nico가 null일 수도 있다고 경고)
+  if (nico != null) {
+    nico.length;
   }
-  if (name2 is int) {
-    // name2는 int 타입이다.
-  }
-
-  // 변수가 어떤 타입인지 모를 때 사용한다.
-  // flutter에서 json을 다룰 때 유용할 수 있다.
-  // 하지만 정말 필요한 경우가 아니라면 사용하지 않는 것이 좋다.
+  nico?.length; // 단축 표현
+  // null safety: 변수 or 데이터가 null일 수도 있다는 것을 명시해주는 것
 }
+
+// dart에서 기본적으로 변수는 not nullable 타입이다.
+// -> null safety를 사용하면 nullable 타입을 사용할 수 있다.
