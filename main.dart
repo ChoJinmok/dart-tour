cde680455d12ac949caee7b9178e5cba2ae96a25
ChoJinmok <jjmok1127@gmail.com>
2023-05-28 17:33:06 +0900

    1.6 Constant Variables

--- main.dart
+++ main.dart
@@ -1,19 +1,16 @@
 void main() {
-  // late
-  // var와 final 같은 키워드 앞에 붙여서 사용
-  // 초기 데이터 없이 변수를 선언할 수 있게 해준다.
-  late var lateVar;
-  late final name;
-  late String name2;
+  // dart의 const는 JS, TS의 const와 다르다
+  // JS, TS의 const는 final과 비슷한 개념
+  // dart의 const는 compile-time constant를 만들어준다.
+  const name = 'nico';
+  // name = 'las'; final과 마찬가지로 한번 할당된 값은 변경할 수 없다.
 
-  late final String name3;
-  // do something, go to api
-  name3 = 'nico';
-  // name3 = 'las'; 여전히 final이기 때문에 한번만 할당 가능
-  // 하지만 late를 사용하면 초기 데이터 없이 변수를 선언할 수 있기 때문에 추후에 할당 가능
+  // const의 가장 큰 특징은 컴파일 시점에 값이 결정되어야 한다는 것이다.
+  const API_KEY = '1234567890'; // 절대 바뀌는 않는 값이면서 컴파일 시점에 값을 알 수 있는 경우에 사용
 
-  // late는 실수를 방지하는 데 도움이 된다.
-  late final String name4;
-  // print(name4); late 변수에 할당하기 전 접근하면 에러 발생
-  // data fetching 시에 유용하다.
+  // 사용자의 휴대폰에서 앱이 실행되면서 정해지는 것들의 경우
+  // const API_KEY = fetchApiKey(); => 컴파일 시점에 값을 알 수 없기 때문에 const 사용 불가
+  // final API_KEY = fetchApiKey(); => 컴파일 시점에 값을 알 수 없기 때문에 final 사용 가능
+
+  // 컴파일 시점(앱을 올리기 전)에 값을 알 수 있는 경우에는 const를 사용하고, 그렇지 않은 경우에는 final을 사용한다.
 }
