6d281c4483f245668fbc86b321cb5c40ab3295ac
ChoJinmok <jjmok1127@gmail.com>
2023-06-05 22:35:39 +0900

    Feature: 3.1 Named Parameters

--- main.dart
+++ main.dart
@@ -1,18 +1,36 @@
-// 함수의 앞에 void를 붙이면 함수는 반환값이 없다는 것을 의미함
-void sayHello(String name) {
-  // 함수는 인사를 출력하는 side effect만 있고 반환값이 없음
-  print('Hello $name nice to meet you!');
+// Named Parameters
+// 함수에 더 많은 정보를 전달할 때 사용
+
+// 다음과 같이 코드를 작성하면 함수 인자 순서를 기억해야함.
+// String sayHello3(String name, int age, String country) {
+//   return 'Hello $name, you are $age years old and you come from $country';
+// }
+
+// void main() {
+//   print(sayHello3('jinmok', 20, 'Korea'));
+// }
+
+// Named Parameters를 사용하면 함수 인자 순서를 기억하지 않아도 됨.
+// 함수 인자 앞에 {}를 붙이면 Named Parameters가 됨.
+// 그런데 {}을 붙이고 나면 인자는 모두 null이 될 수도 있다고 Dart는 인식함.
+// 실제로 유저가 특정 인자를 전달하지 않으면 null이 됨.
+// 방법 1. 인자에 기본값을 주는 방법
+// String sayHello4({String name = 'anonymous', int age = 99, String country = 'nowhere'}) {
+// 방법 2. required 키워드를 사용하는 방법
+String sayHello4({
+  required String name,
+  required int age,
+  required String country,
+}) {
+  return 'Hello $name, you are $age years old and you come from $country';
 }
 
-String sayHello2(String name) {
-  return 'Hello $name nice to meet you!';
-}
-
-// fat arrow syntax: 바로 반환값을 반환하는 함수를 만들 때 사용
-String sayHello3(String name) => 'Hello $name nice to meet you!';
-
-num plus(num a, num b) => a + b;
-
+// 전달해야하는 인자의 순서를 기억할 필요가 없음.
+// 자동완성 기능을 사용할 수 있음, 자료형 추론 기능을 사용할 수 있음.
 void main() {
-  print(sayHello3('jinmok'));
+  print(sayHello4(
+    name: 'jinmok',
+    age: 20,
+    country: 'Korea',
+  ));
 }
