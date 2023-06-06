76629968d0f823c8e73dc2d12a21fb1fc918d5df
ChoJinmok <jjmok1127@gmail.com>
2023-06-07 00:00:05 +0900

    3.4 QQ Operator

--- main.dart
+++ main.dart
@@ -1,15 +1,24 @@
-// Positional Parameters
-// 함수 인자를 순서대로 전달하는 방법
+// QQ Operator: 좌항이 null이면 우항을 반환하고, null이 아니면 좌항을 반환
+String capitalizeName(String? name) => name?.toUpperCase() ?? 'ANONYMOUS';
 
-// Optional Positional Parameters
+// String capitalizeName(String? name) =>
+//     name != null ? name.toUpperCase() : 'ANONYMOUS';
 
-String sayHello(String name, int age, [String country = 'Korea']) {
-  return 'Hello $name, you are $age years old and you come from $country';
-}
+// String capitalizeName(String? name) {
+//   if (name == null) {
+//     return 'ANONYMOUS';
+//   }
+//   return name.toUpperCase();
+// }
 
 void main() {
-  print(sayHello(
-    'jinmok',
-    20,
-  ));
+  capitalizeName('jinmok'); // JINMOK
+  capitalizeName(null);
+
+  // QQ equals, QQ assignment operator
+  String? name;
+  // name이 null이면 'jinmok'을 할당
+  name ??= 'jinmok';
+  name ??= 'another'; // 이미 할당되어 있기 때문에 무시
+  print(name);
 }
