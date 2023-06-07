701b5453747ee41742b7688b2beb2116a2d2d6e8
ChoJinmok <jjmok1127@gmail.com>
2023-06-08 00:13:26 +0900

    3.5 Typedef

--- main.dart
+++ main.dart
@@ -1,24 +1,25 @@
-// QQ Operator: 좌항이 null이면 우항을 반환하고, null이 아니면 좌항을 반환
-String capitalizeName(String? name) => name?.toUpperCase() ?? 'ANONYMOUS';
+// Type Definition
+// 자료형이 햇갈릴 때 도움이 될 alias를 만들어주는 방법
 
-// String capitalizeName(String? name) =>
-//     name != null ? name.toUpperCase() : 'ANONYMOUS';
+typedef ListOfInts = List<int>;
 
-// String capitalizeName(String? name) {
-//   if (name == null) {
-//     return 'ANONYMOUS';
-//   }
-//   return name.toUpperCase();
-// }
+ListOfInts reverseListOfNumbers(ListOfInts list) {
+  var reversed = list.reversed;
+  // reversed 자료형은 Iterable<int> (List와 조금 다름)
+  return reversed.toList();
+}
+
+// 구조화된 자료형(structure data)을 정의하고 싶다면 typedef으로 부족할 수 있음 (아래에서 name이 아니라 다른 키가 들어올 수 있음)
+// => Class 필요
+// typedef는 간단한 자료형에 대해서만 사용
+typedef UserInfo = Map<String, String>;
+
+String sayHi(UserInfo userInfo) {
+  return 'Hi ${userInfo['name']}';
+}
 
 void main() {
-  capitalizeName('jinmok'); // JINMOK
-  capitalizeName(null);
-
-  // QQ equals, QQ assignment operator
-  String? name;
-  // name이 null이면 'jinmok'을 할당
-  name ??= 'jinmok';
-  name ??= 'another'; // 이미 할당되어 있기 때문에 무시
-  print(name);
+  print(reverseListOfNumbers([1, 2, 3]));
+  print(sayHi({'name': 'jinmok'}));
+  // print(sayHi({'ㅁㄴㅇㄹㄴㅁㅇㄹ': 'jinmok'})); // 에러 발생
 }
