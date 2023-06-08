f88341dcd6abecf3924f449878dcc52a98df8b8b
ChoJinmok <jjmok1127@gmail.com>
2023-06-09 00:03:44 +0900

    4.0 First Dart Class

--- main.dart
+++ main.dart
@@ -1,25 +1,27 @@
-// Type Definition
-// 자료형이 햇갈릴 때 도움이 될 alias를 만들어주는 방법
+class Player {
+  // class에서 property를 정의할 때는 타입을 명시해주는 것이 보통
+  // String name = 'jinmok';
+  // property를 변경하지 못하게 하고 싶다면 final 키워드 사용
+  final String name = 'jinmok';
+  int xp = 1500;
 
-typedef ListOfInts = List<int>;
-
-ListOfInts reverseListOfNumbers(ListOfInts list) {
-  var reversed = list.reversed;
-  // reversed 자료형은 Iterable<int> (List와 조금 다름)
-  return reversed.toList();
-}
-
-// 구조화된 자료형(structure data)을 정의하고 싶다면 typedef으로 부족할 수 있음 (아래에서 name이 아니라 다른 키가 들어올 수 있음)
-// => Class 필요
-// typedef는 간단한 자료형에 대해서만 사용
-typedef UserInfo = Map<String, String>;
-
-String sayHi(UserInfo userInfo) {
-  return 'Hi ${userInfo['name']}';
+  // Player가 인사하는 method
+  void sayHello() {
+    // Dart의 class에서는 this 키워드를 사용할 필요 없음 (this를 사용할 수도 있음)
+    // this를 붙이지 않기를 권장
+    // 만약 함수안에 동일한 이름의 변수가 있고 Player class의 name property를 사용하고 싶다면 this.name 사용
+    // var name = 'lizzy';
+    print('Hi my name is $name');
+  }
 }
 
 void main() {
-  print(reverseListOfNumbers([1, 2, 3]));
-  print(sayHi({'name': 'jinmok'}));
-  // print(sayHi({'ㅁㄴㅇㄹㄴㅁㅇㄹ': 'jinmok'})); // 에러 발생
+  // New를 붙일 필요 없음 (사용할 순 있음)
+  var player = Player();
+  // 값을 사용할 수도 있고 변경할 수도 있음
+  print(player.name);
+  // final로 선언된 변수는 변경할 수 없음
+  // player.name = 'lizzy';
+  print(player.name);
+  player.sayHello();
 }
