7bc60ea362e10198561c7facada5cd3adc67f1b1
ChoJinmok <jjmok1127@gmail.com>
2023-06-16 13:01:30 +0900

    4.7 Abstract Class

--- main.dart
+++ main.dart
@@ -1,20 +1,20 @@
-// Enum 타입은 개발자의 실수를 줄여주는 역할을 함
+// Abstract Method
+// 추상화 클래스는 실제 Class처럼 실제 사용하지 않음 (객체를 생성하지 않음)
+// 추상화 클래스는 다른 클래스들이 직접 구현해야하는 메서드들을 모아 놓은 일종의 Blueprint 역할을 함
 
-// 실제로 Flutter에는 옵션들이 다양한 경우가 있음
-// color, margin, padding, alignment, etc...
-// 이런 경우 실수가 발생하기 쉬움
-// ex. flex -> felx
+abstract class Human {
+  // 어떠한 것도 구현하지 않음
+  // 대신 메소드의 시그니처만 정의함
+  void walk();
+}
+// 수만은 청사진에 메서드의 이름과 타입만 정함 (파라미터 타입, 반환 타입)
+// 다른 클래스에서 extends를 통해 구현함
 
-// enum은 선택의 폭을 좁히는 역할을 함
-// 예를 들어 팀 이름은 red, blue만 있는데 reddd라는 값을 넣으면 오류가 발생함
-// 이럴 때 enum을 사용하면 오류를 줄일 수 있음
-
-enum Team { red, blue } // => 새로운 타입을 만들어줌
-// ("")를 붙여줄 필요 없음
+enum Team { red, blue }
 
 enum XPLevel { beginner, medium, pro }
 
-class Player {
+class Player extends Human {
   String name;
   XPLevel xp;
   int age;
@@ -27,27 +27,35 @@ class Player {
     required this.team,
   });
 
+  void walk() {
+    print('$name is walking...');
+  }
+
   void sayHello() {
     print('Hi my name is $name');
   }
 }
 
+class Coach extends Human {
+  void walk() {
+    print('Coach is walking...');
+  }
+}
+
+// Player와 Coach는 Human 클래스를 상속받았기 때문에 walk 메서드를 구현해야함
+// 그렇지만 추상화 클래스이기 때문에 walk 방식을 변경할 수 있음
+
 void main() {
   var jinmok = Player(
     name: 'jinmok',
-    // xp: 1500,
     xp: XPLevel.pro,
     age: 25,
-    // 아래와 같이 오타가 날 수 있음
-    // team: 'redd',
     team: Team.red,
   );
   var lizzy = jinmok
     ..name = 'lizzy'
-    // ..xp = 1000
     ..xp = XPLevel.beginner
     ..age = 24
-    // ..team = 'blue'
     ..team = Team.blue
     ..sayHello();
 
