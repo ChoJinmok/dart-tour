ec8531c4444af902b96b68ea9e7ba818d0f16354
ChoJinmok <jjmok1127@gmail.com>
2023-06-17 18:54:17 +0900

    4.8 Inheritance

--- main.dart
+++ main.dart
@@ -1,65 +1,31 @@
-// Abstract Method
-// 추상화 클래스는 실제 Class처럼 실제 사용하지 않음 (객체를 생성하지 않음)
-// 추상화 클래스는 다른 클래스들이 직접 구현해야하는 메서드들을 모아 놓은 일종의 Blueprint 역할을 함
+class Human {
+  final String name;
 
-abstract class Human {
-  // 어떠한 것도 구현하지 않음
-  // 대신 메소드의 시그니처만 정의함
-  void walk();
-}
-// 수만은 청사진에 메서드의 이름과 타입만 정함 (파라미터 타입, 반환 타입)
-// 다른 클래스에서 extends를 통해 구현함
-
-enum Team { red, blue }
-
-enum XPLevel { beginner, medium, pro }
-
-class Player extends Human {
-  String name;
-  XPLevel xp;
-  int age;
-  Team team;
-
-  Player({
-    required this.name,
-    required this.xp,
-    required this.age,
-    required this.team,
-  });
-
-  void walk() {
-    print('$name is walking...');
-  }
+  Human({required this.name});
 
   void sayHello() {
     print('Hi my name is $name');
   }
 }
 
-class Coach extends Human {
-  void walk() {
-    print('Coach is walking...');
+enum Team { red, blue }
+
+class Player extends Human {
+  final Team team;
+
+  Player({
+    required this.team,
+    required String name,
+  }) : super(name: name);
+
+  @override
+  void sayHello() {
+    super.sayHello();
+    print('and I play for the $team');
   }
 }
 
-// Player와 Coach는 Human 클래스를 상속받았기 때문에 walk 메서드를 구현해야함
-// 그렇지만 추상화 클래스이기 때문에 walk 방식을 변경할 수 있음
-
 void main() {
-  var jinmok = Player(
-    name: 'jinmok',
-    xp: XPLevel.pro,
-    age: 25,
-    team: Team.red,
-  );
-  var lizzy = jinmok
-    ..name = 'lizzy'
-    ..xp = XPLevel.beginner
-    ..age = 24
-    ..team = Team.blue
-    ..sayHello();
-
-  jinmok.sayHello();
-
-  lizzy.sayHello();
+  var player = Player(team: Team.red, name: 'jinmok');
+  player.sayHello();
 }
